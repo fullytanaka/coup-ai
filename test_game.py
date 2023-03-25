e5ae2640398a31bc28c54dfbbcb78cdd9ac980be
Aaron <aaron.monte@kcl.ac.uk>
2023-03-25 20:20:01 +0000

    Refactor fixture to automatically add players

--- test_game.py
+++ test_game.py
@@ -5,18 +5,18 @@ import game
 @pytest.fixture
 def coup():
     coup = game.Game()
+    coup.add_player("Player 1")
+    coup.add_player("Player 2")
     return coup 
 
 def test_can_add_and_remove_player(coup):
-    coup.add_player("Player 1")
-    assert coup.players[0].name == "Player 1"
-    assert coup.players[0].hand == []
-    coup.remove_player("Player 1")
-    assert coup.players == []
+    coup.remove_player("Player 2")
+    assert len(coup.players) == 1
+    coup.add_player("Player 2")
+    assert coup.players[1].name == "Player 2"
+    assert coup.players[1].hand == []
 
 def test_initial_draw_gives_two_cards(coup, monkeypatch):
-    coup.add_player("Player 1")
-    coup.add_player("Player 2")
     monkeypatch.setattr("builtins.input", lambda: 0)
     coup.initial_draw()
     assert len(coup.players[0].hand) == 2
@@ -24,8 +24,6 @@ def test_initial_draw_gives_two_cards(coup, monkeypatch):
 
 def test_coup(coup, capfd, monkeypatch):
     monkeypatch.setattr("builtins.input", lambda: 0)
-    coup.add_player("Player 1")
-    coup.add_player("Player 2")
     coup.players[0].hand = ["duke", "captain"]
     coup.players[1].hand = ["duke"]
 
