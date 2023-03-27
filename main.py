0c73bb42221738ae34cb8047f28899a6362b8773
Aaron <aaron.monte@kcl.ac.uk>
2023-03-27 22:51:53 +0100

    Move game_loop to main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import game
+import random
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -42,4 +43,75 @@ if __name__ == "__main__":
 
     game.initial_draw()
     while game.game_won == False:
-        game.game_loop()
+        game.round += 1
+        random.shuffle(game.deck)
+        print(f"==================== Round {game.round} ====================")
+        for i in range(len(game.players)):
+            influence_count = [len(player.hand) for player in game.players]
+
+            if min(influence_count) == 0: # Check if a player has no more influences
+                print(f"{game.players[influence_count.index(min(influence_count))]} has no more influences!")
+                print("Game over!")
+                print(f"{game.players[influence_count.index(max(influence_count))]} wins!")
+                game.game_won = True
+                break
+
+            print(f"{game.players[i].name}'s turn.")
+            print(game.players[i].print_information())
+
+            # Player chooses an action
+            while True:
+                game.playable_actions = ["coup", "income", "foreign_aid", "tax", "steal", "assassinate", "exchange"]
+                game.challenge_attempted = False
+                game.block_attempted = False
+
+                try:
+                    action = input(f"Choose an action from {', '.join(game.playable_actions)}: ").lower().replace(" ", "_")
+                    if action not in game.playable_actions:
+                        raise ValueError
+
+                    # If the action is challengeable/blockable, target chooses to allow, challenge or block
+                    if action in game.challengeable_actions and action in game.blockable_actions:
+                        game.playable_actions = ["allow", "challenge", "block"]
+                    elif action in game.challengeable_actions:
+                        game.playable_actions = ["allow", "challenge"]
+                    elif action in game.blockable_actions:
+                        game.playable_actions = ["allow", "block"]
+
+                    if action in game.challengeable_actions or action in game.blockable_actions:
+                        print(f"{game.players[(i + 1) % len(game.players)]}, choose to {', '.join(game.playable_actions)}: ")
+                        match input().lower().replace(" ", "_"):
+                            case "challenge":
+                                game.challenge_attempted = True
+                            case "block":
+                                game.block_attempted = True
+                            case default:
+                                pass
+
+                    if game.block_attempted:
+                        # Player can challenge a block attempt
+                        game.playable_actions = ["allow", "challenge"]
+                        match input(f"{game.players[i]}, choose to challenge or allow: "):
+                            case "challenge":
+                                game.challenge_attempted = True
+                            case default:
+                                pass
+                        game.block(game.players[i], game.players[(i + 1) % len(game.players)], action)
+                        break
+                    else: # Action is allowed
+                        if game.challenge_attempted:
+                            if game.challenge(game.players[i], game.players[(i + 1) % len(game.players)], action):
+                                break
+
+                        print(f"{game.players[i].name} chose {action}.")
+                        if action in ["coup", "assassinate", "steal"]: # Actions that require a target
+                            getattr(game, action)(game.players[i], game.players[(i + 1) % len(game.players)])
+                        else:
+                            getattr(game, action)(game.players[i])
+                        break
+
+                except ValueError:
+                    print("Invalid input. Try again.")
+                except Exception as e:
+                    print(f"An error occurred: {e}")
+                    raise e
